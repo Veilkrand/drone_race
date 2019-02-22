12d5c1a8b462aecac6793b82ccb1efb056cc7c64
Chen, Zhuhui <ekschencn@outlook.com>
2019-02-22 23:54:19 +0800

    Applied low-pass filter on cheat velocity report.

--- cascaded_pid_control/src/cheat_odometry_node.cpp
+++ cascaded_pid_control/src/cheat_odometry_node.cpp
@@ -12,11 +12,36 @@
 
 namespace cascaded_pid_control {
 
+  template <typename T>
+  class LowPassFilter {
+    public:
+    explicit inline LowPassFilter(const T& initial_value = 1.0, const T& tau = 1.0) 
+    : initial_(initial_value), value_(initial_value), tau_(tau) { }
+
+    inline void Reset() {
+      value_ = initial_;
+    }
+
+    inline T Update(const T& measurement, double dt) {
+      T alpha = static_cast<T>(dt / (dt + tau_));
+      value_ = (1 - alpha) * value_ + alpha * measurement;
+      return value_;
+    }
+
+    private:
+    T initial_;
+    T value_;
+    T tau_;
+  };
+
   class CheatOdometryNode : public xros::RunnableNode<CheatOdometryNode> {
     public:
     CheatOdometryNode(XROS_CONSTRUCTOR_DEFAULT_PARAMETERS) 
      : XROS_CONSTRUCTOR_INIT_RUNNABLE,
-       tf_listener_(buffer_) { }
+       tf_listener_(buffer_),
+       vx_(0, 0.05),
+       vy_(0, 0.05),
+       vz_(0, 0.05) { }
 
     virtual void Init() {
       ROS_INFO("CheatNode initialized. You should not see this message when in final solution because if so, you're cheating!");      
@@ -46,9 +71,9 @@ namespace cascaded_pid_control {
 	      double dt = current - last_odometry_time_;
         // note: velocities info in twist message is encoded in body frame.
         geometry_msgs::Twist twist_world;
-	      twist_world.linear.x = (odometry.pose.pose.position.x - last_position_.x) / dt;
-	      twist_world.linear.y = (odometry.pose.pose.position.y - last_position_.y) / dt;
-	      twist_world.linear.z = (odometry.pose.pose.position.z - last_position_.z) / dt;
+	      twist_world.linear.x = vx_.Update((odometry.pose.pose.position.x - last_position_.x) / dt, dt);
+	      twist_world.linear.y = vy_.Update((odometry.pose.pose.position.y - last_position_.y) / dt, dt);
+	      twist_world.linear.z = vz_.Update((odometry.pose.pose.position.z - last_position_.z) / dt, dt);
         
         geometry_msgs::TransformStamped transform_stamped_inverse = buffer_.lookupTransform("uav/imu", "world", ros::Time(0));
         tf2::doTransform(twist_world.linear, odometry.twist.twist.linear, transform_stamped_inverse);
@@ -63,6 +88,10 @@ namespace cascaded_pid_control {
     tf2_ros::Buffer buffer_;
     tf2_ros::TransformListener tf_listener_;
     ros::Publisher odometry_pub_;
+
+    LowPassFilter<double> vx_;
+    LowPassFilter<double> vy_;
+    LowPassFilter<double> vz_;
 
     geometry_msgs::Point last_position_;
     double last_odometry_time_;
